67d48f0bc32f0772ece14c81f7881839829f1071
Will Trimble <75294317+wmt1276@users.noreply.github.com>
2022-12-01 00:32:56 -0500

    Completed Day 1

--- 1a.py
+++ 1a.py
@@ -2,5 +2,24 @@ from aoc import get_input
 
 data = get_input(1).splitlines()
 
-for line in data:
-    print(line)
+def main(data: list) -> int:
+    elf_totals = [] # Store Total Calories for Each Elf
+    
+    elf = 0 # Store Total Calories of Current Elf (Initialized to 0)
+    for line in data:   # Iterate Over Each Line in Data
+        if line == "":  # End of Elf's Calories -> Add to elf_totals and Reset elf
+            elf_totals.append(elf)
+            
+            elf = 0
+            
+        else:   # Add Calories to Current Elf
+            elf += int(line)
+            
+    return max(elf_totals)  # Return Maximum Calories
+
+if __name__ == '__main__':
+    # Get Input Data
+    data = get_input(1).splitlines()
+    
+    # Print Answer
+    print(main(data))
