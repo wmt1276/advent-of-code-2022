96379636a336c7d2c379e1db7d1f51bcf855c275
Will Trimble <75294317+wmt1276@users.noreply.github.com>
2022-12-02 00:17:28 -0500

    Raw Solutions

--- 2a.py
+++ 2a.py
@@ -1,10 +1,42 @@
 from aoc import get_input
 
-def main(data: list) -> int:
-    for line in data:   # Iterate Over Each Line in Data
-        print(line)
+dictionary = {'A': 'R', 'B': 'P', 'C': 'S', 'X': 'R', 'Y': 'P', 'Z': 'S'}
+score_dict = {'W': 6, 'T': 3, 'L': 0}
+throw_score = {'R': 1, 'P': 2, 'S': 3}
 
-    return None
+def main(data: list) -> int:
+    score = 0
+    
+    for line in data:   # Iterate Over Each Line in Data
+        line = line.split(' ')
+        outcome = win(dictionary[line[0]], dictionary[line[1]])
+        score += score_dict[outcome]
+        score += throw_score[dictionary[line[1]]]
+
+    return score
+
+def win(opponent, self):
+    if opponent == 'R':
+        if self == 'R':
+            return 'T'
+        elif self == 'P':
+            return 'W'
+        elif self == 'S':
+            return 'L'
+    elif opponent == 'P':
+        if self == 'R':
+            return 'L'
+        elif self == 'P':
+            return 'T'
+        elif self == 'S':
+            return 'W'
+    elif opponent == 'S':
+        if self == 'R':
+            return 'W'
+        elif self == 'P':
+            return 'L'
+        elif self == 'S':
+            return 'T'
 
 if __name__ == '__main__':
     # Get Input Data
--- 2b.py
+++ 2b.py
@@ -1,10 +1,41 @@
 from aoc import get_input
 
-def main(data: list) -> int:
-    for line in data:   # Iterate Over Each Line in Data
-        print(line)
+dictionary = {'A': 'R', 'B': 'P', 'C': 'S', 'X': 'R', 'Y': 'P', 'Z': 'S'}
+score_dict = {'Z': 6, 'Y': 3, 'X': 0}
+throw_score = {'R': 1, 'P': 2, 'S': 3}
 
-    return None
+def throw(opponent, self):
+    if self == 'Y':
+        return opponent
+    else:
+        if opponent == 'R':
+            if self == 'X':
+                return 'S'
+            elif self == 'Z':
+                return 'P'
+        elif opponent == 'P':
+            if self == 'X':
+                return 'R'
+            elif self == 'Z':
+                return 'S'
+        elif opponent == 'S':
+            if self == 'X':
+                return 'P'
+            elif self == 'Z':
+                return 'R'
+
+def main(data: list) -> int:
+    score = 0
+    
+    for line in data:   # Iterate Over Each Line in Data
+        line = line.split(' ')
+        score += score_dict[line[1]]
+        
+        thro = throw(dictionary[line[0]], line[1])
+        
+        score += throw_score[thro]
+
+    return score
 
 if __name__ == '__main__':
     # Get Input Data
