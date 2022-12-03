6b650fb66d2e97740c97324b59d48870300ec68f
Will Trimble <75294317+wmt1276@users.noreply.github.com>
2022-12-03 01:05:28 -0500

    Completed Day 3

--- 3a.py
+++ 3a.py
@@ -1,11 +1,31 @@
 from aoc import get_input
 from aocd import submit
 
-def main(data: list) -> int:
-    for line in data:   # Iterate Over Each Line in Data
-        print(line)
+def priotiy(item):
+    # Returns Priority Score of Item Based on Character
+    if item < '[':
+        return ord(item) - ord('A') + 27
+    else:
+        return ord(item) - ord('a') + 1
+    
+def multiplicity(rucksack):
+    # Finds Item in Both Rucksack Compartments
+    
+    # Split Rucksack Compartments
+    first = rucksack[:len(rucksack)//2]
+    second = rucksack[len(rucksack)//2:]
+    
+    # Find Item in Both Compartments
+    for item in first:
+        if item in second:
+            return item
 
-    return None
+def main(data: list) -> int:
+    priotities = 0
+    for line in data:   # Iterate Over Each Line in Data
+        priotities += priotiy(multiplicity(line))
+
+    return priotities
 
 if __name__ == '__main__':
     # Get Input Data
@@ -16,6 +36,6 @@ if __name__ == '__main__':
     print(answer)
     
     if answer not in [None, '', 0]:
-        submit(answer)
+        submit(answer, part='a', day=3, year=2022)
     else:
         print('No Answer Submitted')
--- 3b.py
+++ 3b.py
@@ -1,11 +1,38 @@
 from aoc import get_input
 from aocd import submit
 
-def main(data: list) -> int:
-    for line in data:   # Iterate Over Each Line in Data
-        print(line)
+def priotiy(item):
+    # Returns Priority Score of Item Based on Character
+    if item < '[':
+        return ord(item) - ord('A') + 27
+    else:
+        return ord(item) - ord('a') + 1
+        
+def find_badges(data: list) -> str:
+    # Find Badge Item in All Rucksacks
+    for char in data[0]:
+        if char in data[1] and char in data[2]:
+            return char
 
-    return None
+def main(data: list) -> int:
+    priotities = 0
+    
+    i = 0
+    rucksacks = []
+    for line in data:   # Iterate Over Each Line in Data
+        rucksacks.append(line)
+        
+        i += 1
+        
+        if i == 3:
+            badge = find_badges(rucksacks)
+            
+            priotities += priotiy(badge)
+            
+            i = 0
+            rucksacks = []
+
+    return priotities
 
 if __name__ == '__main__':
     # Get Input Data
@@ -16,6 +43,6 @@ if __name__ == '__main__':
     print(answer)
     
     if answer not in [None, '', 0]:
-        submit(answer)
+        submit(answer, part='b', day=3, year=2022)
     else:
         print('No Answer Submitted')
