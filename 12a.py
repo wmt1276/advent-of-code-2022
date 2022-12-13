99d720bbc8f6eb4d663d77f0216608ed43181a1f
Will Trimble <75294317+wmt1276@users.noreply.github.com>
2022-12-12 23:50:43 -0500

    Day 12 Raw

--- 12a.py
+++ 12a.py
@@ -4,10 +4,55 @@ from aocd import submit
 day = 12
 
 def main(data: list):
-    for line in data:   # Iterate Over Each Line in Data
-        print(line)
+    height_map = []
+    cursor = (0, 0)
+    goal = (0, 0)
+    
+    for row in range(len(data)):   # Iterate Over Each Line in Data
+        line = data[row]
+        
+        heights = []
+        for col in range(len(line)):
+            char = line[col]
+            
+            if char == 'S':
+                cursor = (row, col)
+                heights.append(0)
+            elif char == 'E':
+                goal = (row, col)
+                heights.append(ord('z') - ord('a'))
+            else:
+                heights.append(ord(char) - ord('a'))
+        height_map.append(heights)
+                
+    queue = [cursor]
+    visited = {cursor: 0}
+    
+    while goal not in visited:
+        consider = queue.pop(0)
+        
+        if consider[0] > 0:
+            up = (consider[0] - 1, consider[1])
+            if up not in visited and height_map[up[0]][up[1]] <= height_map[consider[0]][consider[1]] + 1:
+                queue.append(up)
+                visited[up] = visited[consider] + 1
+        if consider[0] < len(height_map) - 1:
+            down = (consider[0] + 1, consider[1])
+            if down not in visited and height_map[down[0]][down[1]] <= height_map[consider[0]][consider[1]] + 1:
+                queue.append(down)
+                visited[down] = visited[consider] + 1
+        if consider[1] > 0:
+            left = (consider[0], consider[1] - 1)
+            if left not in visited and height_map[left[0]][left[1]] <= height_map[consider[0]][consider[1]] + 1:
+                queue.append(left)
+                visited[left] = visited[consider] + 1
+        if consider[1] < len(height_map[0]) - 1:
+            right = (consider[0], consider[1] + 1)
+            if right not in visited and height_map[right[0]][right[1]] <= height_map[consider[0]][consider[1]] + 1:
+                queue.append(right)
+                visited[right] = visited[consider] + 1
 
-    return None
+    return visited[goal]
 
 if __name__ == '__main__':
     # Get Input Data
--- 13a.py
+++ 13a.py
@@ -1,7 +1,7 @@
 from aoc import get_input
 from aocd import submit
 
-day = 14
+day = 13
 
 def main(data: list):
     for line in data:   # Iterate Over Each Line in Data
